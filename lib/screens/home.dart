fe142e1bb04a0f3d5eb85d37d9819efd696d87e3
Anushka <anushkavermatheprincess@gmail.com>
2020-10-03 20:19:13 +0530

    Homepage UI updated.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:bookish/widgets/OurContainer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -23,7 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
             style: TextStyle(fontFamily: 'Peddana', fontSize: 30.0),
           ),
         ),
-        backgroundColor: Color(0xffffd1d4),
+        backgroundColor: Color(0xfff4acb7),
         elevation: 0.0,
         actions: [
           FlatButton.icon(
@@ -40,6 +41,32 @@ class _HomeScreenState extends State<HomeScreen> {
               Navigator.pop(context);
             },
           ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          SizedBox(
+            height: 40.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: OurContainer(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "The God of Small Things",
+                    style: TextStyle(color: Colors.black, fontSize: 60.0),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: OurContainer(
+              child: Text("2", style: TextStyle(color: Colors.black)),
+            ),
+          )
         ],
       ),
     );
